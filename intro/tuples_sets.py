dee0696d1b57ab0cf94740ca906a8fe928f5f22f
brian-ambani <brainambani1@gmail.com>
2023-03-04 02:31:20 +0300

    updated sets

--- intro/tuples_sets.py
+++ intro/tuples_sets.py
@@ -24,4 +24,29 @@ print(len(fruits))
 
 # A set is a collection which is unordered and unindexed. No duplicate members.
 
+# Create set
+
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# Check if in set
+print('Apples' in fruits_set)
+
+
+# Add to set
+fruits_set.add('Grapes')
+
+# Remove from set
+fruits_set.remove('Grapes')
+
+
+# Add duplicate
+fruits_set.add('Apples')
+
+# Clear
+fruits_set.clear()
+
+# Delete
+del fruits_set
+
+print(fruits_set)
 
