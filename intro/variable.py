9bbf00c33a7b0b5eb550b4b4e3dec464af05e8b4
brian-ambani <brainambani1@gmail.com>
2023-03-03 09:35:41 +0300

    variables

---
+++ intro/variable.py
@@ -0,0 +1,25 @@
+a = 2
+a = a + 3
+
+print(a)
+b = 10
+
+print(a + b)
+
+#this is a single line comment
+'''
+This is a multiline comment.
+'''
+
+'''
+x = 1           #int
+y = 2.5         #float
+name = 'John'   #str
+is_cool = True  #bool
+'''
+
+#multiple assignment
+
+x, y, name, is_cool = (1, 2.5, 'John', True)
+
+print(x, y, name, is_cool)
