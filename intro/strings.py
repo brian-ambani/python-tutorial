01b34c7d32516460db7133b692b08704b365f2e6
brian-ambani <brainambani1@gmail.com>
2023-03-04 01:51:56 +0300

    Handled some string merthods

--- intro/strings.py
+++ intro/strings.py
@@ -13,4 +13,27 @@ age = 25
 #print(f'Hello my name is {name} and I am {age}')
 
 
-#string methods
+# Common string methods
+
+s = 'Hello python'
+
+# Get length
+print(len(s))
+
+# Split
+print(s.split())
+
+# Find position
+print(s.find('y'))
+
+# capitalize string
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+#make all lowercase
+print(s.lower())
+
+#swap case
+print(s.swapcase())
