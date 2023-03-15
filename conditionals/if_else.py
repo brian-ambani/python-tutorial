287baa5835498a16de0ee9a3dd14e1120a06e3f5
brian-ambani <brainambani1@gmail.com>
2023-03-15 12:44:20 +0300

    conditionals

---
+++ conditionals/if_else.py
@@ -0,0 +1,16 @@
+x = 10
+y = 10
+# # Simple if
+
+# if x > y:
+#     print(f'{x} is greater than {y}')
+
+# elif
+
+if x > y:
+    print(f'{x} is greater than {y}')
+elif x == y:
+    print(f'{x} is equal to {y}')
+else:
+    print(f'{x} is less than {y}')
+
