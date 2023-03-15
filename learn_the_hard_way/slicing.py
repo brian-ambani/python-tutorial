2e2a2a8ba55ee42022c955f9db1f2f8eb24a14d8
brian-ambani <brainambani1@gmail.com>
2023-03-15 21:35:43 +0300

    slicing strings

---
+++ learn_the_hard_way/slicing.py
@@ -0,0 +1,5 @@
+my_string = "This is my stirng"
+
+print(my_string[0:4]) #From the start till before the 4th index
+print(my_string[1:7])
+print(my_string[8:len(my_string)]) # From the 8th index till the end
