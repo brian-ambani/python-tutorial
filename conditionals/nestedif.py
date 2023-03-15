8d9b78c05db5a22cf8cde9f718c89dfc9f0fac68
brian-ambani <brainambani1@gmail.com>
2023-03-15 12:50:34 +0300

    use logical operatos instead of nested if

---
+++ conditionals/nestedif.py
@@ -0,0 +1,11 @@
+x = 10
+y = 20
+
+# if x > 2:
+#     if x <= 10:
+#         print(f'{x} is greater than 2 but less than 11')
+
+# instead of using the above do this:
+
+if x > 2 and x <= 10:
+    print(f'{x} is greater than 2 but less than or equal to 10')
