a46c9b6619761588165dec235174720f1431d2df
brian-ambani <brainambani1@gmail.com>
2023-03-15 22:00:12 +0300

    inserting strings within strings

---
+++ learn_the_hard_way/inserting_strings_within_strings.py
@@ -0,0 +1,9 @@
+string1 = "I like %s" % "python"
+print(string1) # 'I like python'
+
+temp = "Educative"
+string2 = "I like %s" % temp
+print(string2) # 'I like educative'
+
+string3 = "I like %s and %s" % ("Python", temp)
+print(string3) # 'I like Python and Educative'
