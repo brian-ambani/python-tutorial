2378fc674083da4abcb4460df1bbdf650c7034d1
brian-ambani <brainambani1@gmail.com>
2023-03-15 21:43:31 +0300

    print the whole string in reverse

--- learn_the_hard_way/reverse_slicing.py
+++ learn_the_hard_way/reverse_slicing.py
@@ -2,3 +2,4 @@ my_string = "This is my string!"
 
 print(my_string[13:2:-1]) # Take 1 step back each time
 print(my_string[17:0:-2]) # Take 2 steps back.
+print(my_string[::-1]) # the whole string in reverse
