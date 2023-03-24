c979dac0751a9d53be4df21c51d7893528cbcd7c
brian-ambani <brainambani1@gmail.com>
2023-03-24 18:38:09 +0300

    iterate a list

---
+++ iterating/iterate.py
@@ -0,0 +1,13 @@
+# iterating using for-loop
+
+x = [7, 8, 3]
+
+for item in x:
+    print(item)
+
+# iNDEX AND ITEM
+
+print('Print index and item: ')
+x = [7, 8, 3]
+for index, item in enumerate(x):
+    print(index, item)
